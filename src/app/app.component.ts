77cd307d2e99af0344ebbd75d086a97772dadba7
Nimra Amjad <nimra.bscs2020@gmail.com>
2024-03-01 16:24:20 -0800

    get request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { UserService } from './services/user.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'http-service-angular';
+  pageTitle: String = 'Angular HTTP Client';
+  users = new Array<any>();
+  constructor(private userService: UserService) { }
+  ngOnInit(): void {
+    this.userService.getUsers().subscribe((data) => {
+      this.users = data;
+    })
+  }
 }
